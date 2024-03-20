c4787819ee65abcae0875b2b60d84cfdbabd1034
Niranjan <kneerunjun@gmail.com>
2024-03-20 17:50:56 +0530

    - #2  MongoConnect now uses new error framework

--- midlwr.go
+++ midlwr.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/eensymachines-in/patio-web/httperr"
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -64,32 +63,23 @@ func RabbitConnectWithChn(name string) gin.HandlerFunc {
 // MongoConnect : will connect to a mongo database and insert the connection to downstream handlers
 func MongoConnect(c *gin.Context) {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	// credential := options.Credential{
-	// 	AuthMechanism: "PLAIN",
-	// 	Username:      db_USER,
-	// 	Password:      db_PASSWD,
-	// }
-	// defer cancel()
-	// client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:27017", db_SERVER)).SetAuth(credential))
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:27017", db_USER, db_PASSWD, db_SERVER)))
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"err": err,
-		}).Error("failed database conenction: MongoConnect")
-		c.JSON(http.StatusBadGateway, gin.H{
-			"data": "we are facing connectivity problems for now, try again later",
-		})
-	}
-	if client.Ping(ctx, readpref.Primary()) != nil {
-		logrus.WithFields(logrus.Fields{
-			"err": err,
-		}).Error("failed database ping: MongoConnect")
-		c.JSON(http.StatusBadGateway, gin.H{
-			"data": "we are facing issues with data connection, try back again",
-		})
+		httperr.HttpErrOrOkDispatch(c, httperr.ErrGatewayConnect(err), log.WithFields(log.Fields{
+			"stack":  "MongoConnect",
+			"login":  db_USER,
+			"server": db_SERVER,
+		}))
 		return
 	}
-	logrus.Debug("connected to database..")
+	if client.Ping(ctx, readpref.Primary()) != nil {
+		httperr.HttpErrOrOkDispatch(c, httperr.ErrGatewayConnect(err), log.WithFields(log.Fields{
+			"stack":  "MongoConnect",
+			"login":  db_USER,
+			"server": db_SERVER,
+		}))
+		return
+	}
 	c.Set("mongo-client", client)
 }
 
